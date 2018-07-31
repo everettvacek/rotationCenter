a5e0db194da4ae2601101022372eb7e81ca47aad
Everett Vacek <everett@xrm.phys.northwestern.edu>
2018-07-31 17:43:14 -0500

    fixed add_noise to work with real data

--- utils.py
+++ utils.py
@@ -1,12 +1,21 @@
 import numpy as np
 
-def snr_calc(array, noise, exposure):
-    snr = np.mean(array/np.max(array)*exposure)/np.std(noise-array/np.max(array)*exposure)
+def snr_calc(array, noise, exposure=None):
+    if exposure == None:
+        snr = np.mean(array)/np.std(noise-array)
+    else:
+        snr = np.mean(array/np.max(array)*exposure)/np.std(noise-array/np.max(array)*exposure)
     return snr
 
-def add_noise(array, exposure, bit_depth = 8):
-    noise = np.random.poisson(array/np.max(array)*exposure)
-    snr = snr_calc(array, noise, exposure) 
+def add_noise(array, exposure = None, bit_depth = None):
+    if exposure == None:
+        noise = array #np.random.poisson(array/np.max(array)*exposure)
+        snr = snr_calc(array, noise, exposure)
+    else:
+        noise = np.random.poisson(array/np.max(array)*exposure)
+        snr = snr_calc(array, noise, exposure)
+    if bit_depth == None:
+        pass
     if bit_depth == 8:
         noise = np.array(np.floor(noise)).astype(dtype=np.uint8)
         #noise = np.array(np.floor(noise*(2**8-2)/np.max(noise))).astype(dtype=np.uint8)
